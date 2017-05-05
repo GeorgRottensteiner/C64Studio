27576e87604e5e168e2625626d77990d2f9ea30a
GeorgRottensteiner <georg@georg-rottensteiner.de>
2017-05-05 21:36:00 +0200

    added license and readme

--- C64Studio/FormLicense.Designer.cs
+++ C64Studio/FormLicense.Designer.cs
@@ -28,7 +28,7 @@
     /// </summary>
     private void InitializeComponent()
     {
-      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager( typeof( FormLicense ) );
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLicense));
       this.btnOK = new System.Windows.Forms.Button();
       this.editLicenses = new System.Windows.Forms.TextBox();
       this.SuspendLayout();
@@ -36,34 +36,34 @@
       // btnOK
       // 
       this.btnOK.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this.btnOK.Location = new System.Drawing.Point( 437, 498 );
+      this.btnOK.Location = new System.Drawing.Point(437, 498);
       this.btnOK.Name = "btnOK";
-      this.btnOK.Size = new System.Drawing.Size( 75, 23 );
+      this.btnOK.Size = new System.Drawing.Size(75, 23);
       this.btnOK.TabIndex = 0;
       this.btnOK.Text = "OK";
       this.btnOK.UseVisualStyleBackColor = true;
-      this.btnOK.Click += new System.EventHandler( this.btnOK_Click );
+      this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
       // 
       // editLicenses
       // 
-      this.editLicenses.Location = new System.Drawing.Point( 12, 12 );
+      this.editLicenses.Location = new System.Drawing.Point(12, 12);
       this.editLicenses.Multiline = true;
       this.editLicenses.Name = "editLicenses";
       this.editLicenses.ReadOnly = true;
       this.editLicenses.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-      this.editLicenses.Size = new System.Drawing.Size( 500, 480 );
+      this.editLicenses.Size = new System.Drawing.Size(500, 480);
       this.editLicenses.TabIndex = 1;
-      this.editLicenses.Text = resources.GetString( "editLicenses.Text" );
+      this.editLicenses.Text = resources.GetString("editLicenses.Text");
       // 
       // FormLicense
       // 
       this.AcceptButton = this.btnOK;
-      this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.CancelButton = this.btnOK;
-      this.ClientSize = new System.Drawing.Size( 524, 533 );
-      this.Controls.Add( this.editLicenses );
-      this.Controls.Add( this.btnOK );
+      this.ClientSize = new System.Drawing.Size(524, 533);
+      this.Controls.Add(this.editLicenses);
+      this.Controls.Add(this.btnOK);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
       this.MaximizeBox = false;
       this.MinimizeBox = false;
@@ -72,7 +72,7 @@
       this.ShowInTaskbar = false;
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
       this.Text = "Licenses";
-      this.ResumeLayout( false );
+      this.ResumeLayout(false);
       this.PerformLayout();
 
     }
---
+++ readme.md
@@ -0,0 +1,16 @@
+C64 Studio is a .NET based IDE.
+
+The program supports project based C64 assembly and/or Basic V2 and is geared towards game development.
+
+C64 Studio is a .NET based IDE.
+
+The program supports project based C64 assembly or Basic V2. The internal assembler is using the ACME syntax.
+
+In connection with Vice the IDE allows you to debug through your code and watch variables/memory locations, registers and memory.
+Any other emulator can be set up as well if it's startable via runtime arguments.
+
+C64 Studio allows you to compile to raw binary, .prg, .t64, .d64 or cartridge format (.bin and .crt for 8k, 16k, MagicDesk)
+
+Additionally to this C64 Studio comes with a charset, sprite and media editor (tape and disk).
+
+An encompassing help documentation is part of the program. 
