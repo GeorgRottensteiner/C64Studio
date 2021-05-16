218c293a3a8a609029c77d1372887a04f8d48c0e
Georg Rottensteiner <georg@georg-rottensteiner.de>
2021-05-16 18:34:59 +0200

    Add: Partial support for Charpad ctm format version 7

--- C64Models/Formats/CharpadProject.cs
+++ C64Models/Formats/CharpadProject.cs
@@ -98,6 +98,7 @@ namespace C64Studio.Formats
 
 
     public int                    BackgroundColor = 0;
+    public int                    BackgroundColor4 = 4;
     public int                    MultiColor1 = 8;
     public int                    MultiColor2 = 9;
     public int                    CustomColor = 1;
@@ -110,6 +111,7 @@ namespace C64Studio.Formats
     public int                    MapWidth = 0;
     public int                    MapHeight = 0;
     public ColorMode              TileColorMode = ColorMode.GLOBAL;
+    public Types.CharsetMode      CharsetMode = Types.CharsetMode.HIRES;
 
     public List<SingleChar>       Characters = new List<SingleChar>();
     public List<Tile>             Tiles = new List<Tile>();
@@ -133,17 +135,27 @@ namespace C64Studio.Formats
       }
       int version = Data.ByteAt( 3 );
       if ( ( version != 4 )
-      &&   ( version != 5 ) )
+      &&   ( version != 5 )
+      &&   ( version != 6 )
+      &&   ( version != 7 ) )
       {
-        System.Windows.Forms.MessageBox.Show( "Currently only version 4 or 5 of Charpad project files is supported. Sorry!", "Unsupported version " + version );
+        System.Windows.Forms.MessageBox.Show( "Currently only version 4, 5, 6 or 7 of Charpad project files is supported. Sorry!", "Unsupported version " + version );
         return false;
       }
 
-      if ( version == 4 )
+      switch ( version )
       {
-        return LoadVersion4( Data );
+        case 4:
+          return LoadVersion4( Data );
+        case 5:
+          return LoadVersion5( Data );
+        case 6:
+          // TODO!
+          return LoadVersion5( Data );
+        case 7:
+        default:
+          return LoadVersion7( Data );
       }
-      return LoadVersion5( Data );
     }
 
 
@@ -490,6 +502,328 @@ namespace C64Studio.Formats
       }
       return true;
     }
+
+
+
+    private bool LoadVersion7( GR.Memory.ByteBuffer Data )
+    {
+      BackgroundColor = Data.ByteAt( 4 );
+      MultiColor1 = Data.ByteAt( 5 );
+      MultiColor2 = Data.ByteAt( 6 );
+      BackgroundColor4 = Data.ByteAt( 7 );
+      CustomColor = Data.ByteAt( 8 );
+      TileColorMode = (ColorMode)Data.ByteAt( 9 );
+
+      CharsetMode = (Types.CharsetMode)Data.ByteAt( 10 );
+
+      byte  flags = Data.ByteAt( 11 );
+      bool  tileSysEnabled = ( ( flags & 0x01 ) != 0 );
+
+
+      ushort charDataBlockID      = 0xdab0;
+      ushort charAttributeBlockID = 0xdab1;
+      ushort mapDataBlockID       = 0xdab2;
+
+
+      if ( !tileSysEnabled )
+      {
+        // fake tiles (one per char)
+        TileWidth   = 1;
+        TileHeight  = 1;
+        NumTiles    = 256;
+
+        for ( int i = 0; i < NumTiles; ++i )
+        {
+          Tile tile = new Tile();
+
+          tile.CharData.Resize( (uint)( TileWidth * TileHeight * 2 ) );
+          tile.CharData.SetU16At( 0, (ushort)i );
+
+          tile.ColorData.Resize( (uint)( TileWidth * TileHeight ) );
+          tile.ColorData.SetU8At( 0, (byte)CustomColor );
+
+          Tiles.Add( tile );
+        }
+      }
+
+      var reader = Data.MemoryReader();
+
+      reader.Skip( 12 );
+
+      while ( reader.DataAvailable )
+      {
+        ushort  blockID = reader.ReadUInt16NetworkOrder();
+
+        if ( blockID == mapDataBlockID )
+        {
+          if ( tileSysEnabled )
+          {
+            // Tile data block
+
+            // TILECNT: Tile count minus one(16 - bit, LSBF).
+            NumTiles = reader.ReadUInt16() + 1;
+
+            // TILEWID: Tile width( byte).
+            TileWidth = reader.ReadUInt8();
+            // TILEHEI: Tile height( byte).
+            TileHeight = reader.ReadUInt8();
+
+            // TILEDAT: Tile data, 16 bits per tile cell( LSBF) for TILEWID* TILEHEI cells * TILECNT items, cells are in LRTB order.
+            for ( int i = 0; i < NumTiles; ++i )
+            {
+              Tile tile = new Tile();
+
+              tile.CharData.Resize( (uint)( TileWidth * TileHeight * 2 ) );
+              tile.ColorData.Resize( (uint)( TileWidth * TileHeight ) );
+
+              Tiles.Add( tile );
+            }
+            if ( NumChars < 256 )
+            {
+              // add all chars for safety reasons
+              for ( int i = NumChars; i < 256; ++i )
+              {
+                SingleChar    newChar = new SingleChar();
+                newChar.Data = new GR.Memory.ByteBuffer( 8 );
+                Characters.Add( newChar );
+              }
+            }
+            for ( int i = 0; i < NumTiles; ++i )
+            {
+              for ( int j = 0; j < TileWidth * TileHeight; ++j )
+              {
+                Tiles[i].CharData.SetU16At( j * 2, reader.ReadUInt16() );
+              }
+            }
+          }
+          else
+          {
+            // BLKMARK : Block marker (0xDA, 0xBn). 
+            // MAPWID: Map Width(16 - bit, LSBF).
+            // MAPHEI: Map height(16 - bit, LSBF). 
+            // MAPDAT: Map data, 16 bits per cell( LSBF ) for MAPWID* MAPHEI cells, cells are in LRTB order.
+            MapWidth = reader.ReadUInt16();
+            MapHeight = reader.ReadUInt16();
+
+            MapData = new GR.Memory.ByteBuffer( (uint)( MapWidth * MapHeight ) );
+
+            for ( int i = 0; i < MapHeight; ++i )
+            {
+              for ( int j = 0; j < MapWidth; ++j )
+              {
+                MapData.SetU8At( i * MapWidth + j, (byte)reader.ReadUInt16() );
+              }
+            }
+          }
+        }
+
+        if ( blockID == charDataBlockID )
+        {
+          // Character data block
+
+          // CHARCNT: Character image count minus one( 16 - bit, LSBF ).
+          NumChars = reader.ReadUInt16() + 1;
+
+          // CHARDAT : Character image data( eight bytes / rows per image for CHARCNT images, rows are in TB order ).
+          for ( int charIndex = 0; charIndex < NumChars; ++charIndex )
+          {
+            SingleChar    newChar = new SingleChar();
+            newChar.Data = new GR.Memory.ByteBuffer();
+            newChar.Color = CustomColor;
+            reader.ReadBlock( newChar.Data, 8 );
+            Characters.Add( newChar );
+          }
+
+          if ( !tileSysEnabled )
+          {
+            if ( NumChars < 256 )
+            {
+              // add all chars for safety reasons
+              for ( int i = NumChars; i < 256; ++i )
+              {
+                SingleChar    newChar = new SingleChar();
+                newChar.Data = new GR.Memory.ByteBuffer( 8 );
+                newChar.Color = CustomColor;
+
+                Characters.Add( newChar );
+              }
+            }
+          }
+        }
+        else if ( blockID == charAttributeBlockID )
+        {
+          // char attributes
+          // BLKMARK: Block marker(0xDA, 0xB1).
+          // CHARATTS: Char attribute data, one byte per char image for CHARCNT images, low nybble = colour, high nybble = material.
+          //           nb.colours are only stored when the colouring mode is "per character".
+          for ( int charIndex = 0; charIndex < NumChars; ++charIndex )
+          {
+            if ( TileColorMode == ColorMode.PER_TILE_CELL )
+            {
+              Characters[charIndex].Color = reader.ReadUInt8() & 0x0f;
+              if ( !tileSysEnabled )
+              {
+                Tiles[charIndex].ColorData.SetU8At( 0, (byte)Characters[charIndex].Color );
+              }
+            }
+          }
+        }
+
+      }
+
+
+      /*
+      
+      
+      int headerSize = 20;
+      int offsetToCharData = headerSize;
+      int offsetToCharAttribs = offsetToCharData + NumChars * 8;
+      int offsetToTileData = offsetToCharAttribs + NumChars;
+      int offsetToTileColors = offsetToTileData + NumTiles * TileWidth * TileHeight * 2;
+      if ( ( tileSysEnabled )
+      || ( noTiles ) )
+      {
+        offsetToTileColors = offsetToTileData;
+      }
+      int offsetToMapData = offsetToTileColors + NumTiles;
+      if ( ( TileColorMode != ColorMode.PER_TILE )
+      || ( noTiles ) )
+      {
+        offsetToMapData = offsetToTileColors;
+      }
+
+      // tile_data
+      if ( noTiles )
+      {
+        for ( int i = 0; i < NumTiles; ++i )
+        {
+          Tile tile = new Tile();
+
+          tile.CharData.Resize( (uint)( TileWidth * TileHeight * 2 ) );
+          tile.CharData.SetU16At( 0, (ushort)i );
+
+          tile.ColorData.Resize( (uint)( TileWidth * TileHeight ) );
+          tile.ColorData.SetU8At( 0, (byte)CustomColor );
+
+          Tiles.Add( tile );
+        }
+        if ( NumChars < 256 )
+        {
+          // add all chars for safety reasons
+          for ( int i = NumChars; i < 256; ++i )
+          {
+            SingleChar    newChar = new SingleChar();
+            newChar.Data = new GR.Memory.ByteBuffer( 8 );
+            if ( TileColorMode == ColorMode.PER_TILE_CELL )
+            {
+              newChar.Color = Data.ByteAt( offsetToCharAttribs + i ) & 0x0f;
+            }
+            else
+            {
+              newChar.Color = CustomColor;
+            }
+
+            Characters.Add( newChar );
+          }
+        }
+      }
+      else
+      {
+        for ( int i = 0; i < NumTiles; ++i )
+        {
+          Tile tile = new Tile();
+
+          tile.CharData.Resize( (uint)( TileWidth * TileHeight * 2 ) );
+          tile.ColorData.Resize( (uint)( TileWidth * TileHeight ) );
+
+          Tiles.Add( tile );
+        }
+
+        if ( tileSysEnabled )
+        {
+          byte curCharIndex = 0;
+          for ( int i = 0; i < NumTiles; ++i )
+          {
+            for ( int j = 0; j < TileWidth * TileHeight; ++j )
+            {
+              Tiles[i].CharData.SetU16At( j * 2, curCharIndex );
+              ++curCharIndex;
+            }
+          }
+        }
+        else
+        {
+          // tile_data.      Size = NUM_TILES * TILE_WIDTH * TILE_HEIGHT bytes * 2 bytes. (only exists if CHAR_DATA is not "Expanded")
+          for ( int i = 0; i < NumTiles; ++i )
+          {
+            for ( int j = 0; j < TileWidth * TileHeight; ++j )
+            {
+              Tiles[i].CharData.SetU16At( j * 2, Data.UInt16At( offsetToTileData + i * TileWidth * TileHeight * 2 + j * 2 ) );
+            }
+          }
+        }
+      }
+
+      // TILE_COLOURS.   Size = NUM_TILES bytes (1 byte per tile = "RAM colour". only exists if COLOR_MODE = 1 (Per Tile)
+      if ( TileColorMode == ColorMode.PER_TILE )
+      {
+        for ( int i = 0; i < NumTiles; ++i )
+        {
+          for ( int y = 0; y < TileHeight; ++y )
+          {
+            for ( int x = 0; x < TileWidth; ++x )
+            {
+              Tiles[i].ColorData.SetU8At( x + y * TileWidth, (byte)( Data.ByteAt( offsetToTileColors + i ) & 0x0f ) );
+            }
+          }
+        }
+      }
+      else if ( TileColorMode == ColorMode.PER_TILE_CELL )
+      {
+        // with V5 this actually means per character
+        for ( int i = 0; i < NumTiles; ++i )
+        {
+          for ( int y = 0; y < TileHeight; ++y )
+          {
+            for ( int x = 0; x < TileWidth; ++x )
+            {
+              byte    charColor = (byte)Characters[Tiles[i].CharData.ByteAt( 2 * ( x + y * TileWidth ) )].Color;
+              Tiles[i].ColorData.SetU8At( x + y * TileWidth, charColor );
+            }
+          }
+        }
+      }
+      else if ( TileColorMode == ColorMode.GLOBAL )
+      {
+        for ( int i = 0; i < NumTiles; ++i )
+        {
+          for ( int y = 0; y < TileHeight; ++y )
+          {
+            for ( int x = 0; x < TileWidth; ++x )
+            {
+              Tiles[i].ColorData.SetU8At( x + y * TileWidth, (byte)CustomColor );
+            }
+          }
+        }
+      }
+      else if ( TileColorMode == ColorMode.PER_TILE_CELL )
+      {
+        for ( int i = 0; i < NumTiles; ++i )
+        {
+          for ( int y = 0; y < TileHeight; ++y )
+          {
+            for ( int x = 0; x < TileWidth; ++x )
+            {
+              Tiles[i].ColorData.SetU8At( x + y * TileWidth, (byte)( Data.ByteAt( offsetCellAttribs + i * TileWidth * TileHeight + x + y * TileHeight ) & 0x0f ) );
+            }
+          }
+        }
+      }
+
+      */
+      return true;
+    }
+
 
   }
 }
