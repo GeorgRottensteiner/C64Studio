9f35588a2368d4f38a5621719a37d240be5acd95
Georg Rottensteiner <georg@georg-rottensteiner.de>
2021-12-09 20:08:00 +0100

    Fix: File manager showing proper PETSCII characters for file names

--- C64Studio/Documents/FileManager.cs
+++ C64Studio/Documents/FileManager.cs
@@ -224,7 +224,19 @@ namespace C64Studio
       {
         foreach ( C64Studio.Types.FileInfo file in files )
         {
-          string filename = Util.FilenameToUnicode( file.Filename );
+          ByteBuffer  displayFilename = new ByteBuffer( file.Filename );
+
+          // map to PETSCII range
+          for ( int i = 0; i < displayFilename.Length; ++i )
+          {
+            byte    singleByte = displayFilename.ByteAt( i );
+            var c64Char = ConstantData.FindC64KeyByPETSCII( singleByte );
+            if ( c64Char != null )
+            {
+              displayFilename.SetU8At( i, c64Char.PetSCIIValue );
+            }
+          }
+          string filename = Util.FilenameToUnicode( displayFilename );
 
           ListViewItem item = new ListViewItem( filename );
           item.SubItems.Add( file.Blocks.ToString() );
