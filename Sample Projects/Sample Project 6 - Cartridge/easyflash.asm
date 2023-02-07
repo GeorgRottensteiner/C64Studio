8696f35b6ba6db9666ee241454f0a00cdd482705
Georg Rottensteiner <georg@georg-rottensteiner.de>
2023-02-07 19:35:04 +0100

    Fix: easyflash cartridge sample to actually work (Ultimax mode!)

--- Sample Projects/Sample Project 6 - Cartridge/easyflash.asm
+++ Sample Projects/Sample Project 6 - Cartridge/easyflash.asm
@@ -1,8 +1,40 @@
+;Easyflash cartridges start in Ultimax Mode
+;the lower bank is mapped at $8000
+;the higher bank is mapped at $e000
+;Kernal is switched off!
+;this means for the launcher, it has to put a vector
+;to the startup code at $fffc!
+
 !to "easyflash.crt",easyflashcrt
 
 * = $8000
 
-        lda #$0
-        sta 53280
+;first lower bank
+
+!bank 0,$2000
+
+          !word Init
+          !word Init
+          !byte $c3 ;c
+          !byte $c2 ;b
+          !byte $cd ;m
+          !byte $38 ;8
+          !byte $30 ;0
+
+Init
+        sei
+
+
 -
-        jmp - 
+        inc $d020
+        jmp -
+
+
+;first upper bank
+!bank 1,$2000
+!pseudopc $e000
+
+          !fill $fffc - $e000,0
+          !word Init
+
+!realpc
