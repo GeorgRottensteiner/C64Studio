34400e5e6e332cafdeb7f0e3a5ca91400eda8f4c
Georg Rottensteiner <georg@georg-rottensteiner.de>
2023-12-16 07:25:42 +0100

    Fix: introduced bug in !text that would miscalc the number of bytes used

---
+++ Sample Projects/MacroTest/hi_lo_byte_operand.asm
@@ -0,0 +1,6 @@
+* = $1000
+
+.portal_url_size = $2000
+
+!text <.portal_url_size, >.portal_url_size    ;-> Diese Version wird mit der neuen Version vom c64Studio nicht korrekt compiliert
+!text "R", $01, $1c, $00                                ; -> Wenn ich das so mache, geht es auch damit
