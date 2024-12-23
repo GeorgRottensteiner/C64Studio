2ac6731fd3915c1f38dea92a7cad3ac3e452f069
Georg Rottensteiner <georg@georg-rottensteiner.de>
2024-12-23 11:26:03 +0100

    Add: BASIC65 dialect, << and >> tokens

--- C64Studio/Program.cs
+++ C64Studio/Program.cs
@@ -18,25 +18,76 @@ namespace RetroDevStudio
     [STAThread]
     static void Main( string[] args )
     {
+      // Forum64 checksummer attempt
       /*
-      CPCDSK  disk = new CPCDSK();
+      int x = 0;
+      int fd = 0;
+      byte quoteFlag = 0;
+      byte fb = 0;
+      byte fc = 0;
+      var memory = new ByteBuffer( "0A009900" );
+      while ( true )
+      {
+        fd = memory.ByteAt( x );
+        if ( fd == 0 )
+        {
+          //DisplayCheckSum( fb, fc );
+          break;
+        }
+        if ( ( quoteFlag & 0x80 ) != 0 )
+        {
+          if ( fd == 0x22 ) // ASCII for "
+          {
+            quoteFlag ^= 0xFF;
+          }
+        }
+        else
+        {
+          if ( fd == 0x20 ) // ASCII for space
+          {
+            x++;
+            continue;
+          }
+          if ( fd == 0x22 ) // ASCII for "
 
-      disk.CreateEmptyMedia();
+          {
+            quoteFlag ^= 0xFF;
+          }
+        }
+        for ( int y = 8; y > 0; y-- )
+        {
+          fd <<= 1;
+          fd |= (byte)( ( fb & 0x80 ) >> 7 );
+          fb <<= 1;
+          fb |= (byte)( ( fc & 0x80 ) >> 7 );
+          fc <<= 1;
+          if ( ( fd & 0x100 ) != 0 )
+          {
+            fc ^= 0x68;
+          }
+        }
+        x++;
+      }
+      */
+            /*
+            CPCDSK  disk = new CPCDSK();
 
-      ByteBuffer  file = new ByteBuffer( "0D000A00BF2268656C6C6F22000000" );
+            disk.CreateEmptyMedia();
 
-      ByteBuffer  fileToInsert = GR.IO.File.ReadAllBytes( @"C:\Users\Georg\Desktop\CPC\FRUITY.BIN" );
-      //disk.WriteFile( new ByteBuffer( "42415349432020204E424153" ), file, Types.FileType.PRG );
-      disk.WriteFile( new ByteBuffer( "46525549545920204E42494E" ), fileToInsert, Types.FileType.PRG );
-      Debug.Log( disk.LastError );
-      disk.Save( @"C:\Users\Georg\Desktop\CPC\diskwithfilefruitybin.dsk" );*/
-      
+            ByteBuffer  file = new ByteBuffer( "0D000A00BF2268656C6C6F22000000" );
+
+            ByteBuffer  fileToInsert = GR.IO.File.ReadAllBytes( @"C:\Users\Georg\Desktop\CPC\FRUITY.BIN" );
+            //disk.WriteFile( new ByteBuffer( "42415349432020204E424153" ), file, Types.FileType.PRG );
+            disk.WriteFile( new ByteBuffer( "46525549545920204E42494E" ), fileToInsert, Types.FileType.PRG );
+            Debug.Log( disk.LastError );
+            disk.Save( @"C:\Users\Georg\Desktop\CPC\diskwithfilefruitybin.dsk" );*/
+
 
 #if !DEBUG
       try
       {
 #endif
-        if ( Environment.OSVersion.Version.Major >= 6 )
+            if ( Environment.OSVersion.Version.Major >= 6 )
         {
           SetProcessDPIAware();
         }
