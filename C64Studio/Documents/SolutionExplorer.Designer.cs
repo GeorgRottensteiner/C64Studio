4b7e74fd5267087631648e206ed302a402bb73b7
Georg Rottensteiner <georg@georg-rottensteiner.de>
2025-01-22 19:30:58 +0100

    Cosmetic checkin

--- C64Studio/Documents/SolutionExplorer.Designer.cs
+++ C64Studio/Documents/SolutionExplorer.Designer.cs
@@ -61,7 +61,7 @@
       this.treeProject.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this.treeProject.BorderStyle = DecentForms.BorderStyle.SUNKEN;
+      this.treeProject.BorderStyle = DecentForms.BorderStyle.FLAT;
       this.treeProject.ImageList = this.imageListExplorer;
       this.treeProject.LabelEdit = true;
       this.treeProject.Location = new System.Drawing.Point(0, 28);
