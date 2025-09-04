5cb077a7edde68cfb105a305d381e1d2170ae9e1
Georg Rottensteiner <georg@georg-rottensteiner.de>
2025-09-04 17:27:11 +0200

    version update

--- C64Models/Version/StudioVersion.cs
+++ C64Models/Version/StudioVersion.cs
@@ -2,7 +2,7 @@ namespace RetroDevStudio
 {
   public static class Version
   {
-    public const string       VersionBase = "8.1";
+    public const string       VersionBase = "8.2";
     public const string       BuildNumber = "260";
   }
 }
