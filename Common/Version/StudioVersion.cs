018c01f753777ffefb8b08d31c99db150be09a68
Georg Rottensteiner <georg@georg-rottensteiner.de>
2026-09-19 13:56:11 +0200

    Update Release Notes/Version

--- Common/Version/StudioVersion.cs
+++ Common/Version/StudioVersion.cs
@@ -2,7 +2,7 @@ namespace RetroDevStudio
 {
   public static class Version
   {
-    public const string       VersionBase = "8.4";
+    public const string       VersionBase = "8.5";
     public const string       BuildNumber = "260";
   }
 }
